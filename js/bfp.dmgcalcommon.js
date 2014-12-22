24a0682f087775e507d1f6b9843fd721bca4318c
Sam GreenP <greenpuck55@gmail.com>
2014-12-23 07:53:43 +1100

    Create bfp.dmgcalcommon.js

---
+++ js/bfp.dmgcalcommon.js
@@ -0,0 +1,25 @@
+/*Check filedate*/
+function checkUpdate(fileURL,localDate) {
+    $.ajax({
+    	type: "Head",
+        dataType: "json",
+        contentType: "text/plain; charset=UTF-8",
+        url: fileURL,
+        success: function(data, textStatus, request) {
+    		lastModified = request.getResponseHeader("Last-Modified");
+    		if (Date.parse(lastModified) > Date.parse(localDate))
+    			$("#alertmodal").modal("show")
+ 	}
+    })
+}
+
+/*Reset Local Storage*/
+function resetLocalData(serverStr, corrupted) {
+	/*LocalStorage Delete and Refresh*/
+	if (corrupted)
+		alert("Corrupted data detected or Data structure has changed. Proceed to Fix.");
+	localStorage.removeItem(serverStr);
+	localStorage.removeItem(serverStr+"date");
+	history.pushState(null, null, location.pathname);
+	location.reload(true);
+}
