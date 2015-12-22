635694076f73fe3309408c9235622f384de6cc9d
Sam GreenP <greenpuck55@gmail.com>
2015-12-23 03:19:04 +1100

    Update bfp.squadbuildbeta.js

--- js/bfp.squadbuildbeta.js
+++ js/bfp.squadbuildbeta.js
@@ -580,10 +580,10 @@ function generateSummary() {
 					alert("data-found "+lsKey);
 					var tArray=$(this).parent().attr("data-found").split(',');
 					for (j in tArray) {
-						var scanArray=rawParseObj[j].ls.effects;
+						var scanArray=rawParseObj[tArray[j]].ls.effects;
 						for (k in scanArray)
 							if (scanArray[k].hasOwnProperty([lsMap[lsMapKey].impact])) {
-								alert("scanArray[k][lsMap[lsMapKey].impact]");
+								alert("value is "+scanArray[k][lsMap[lsMapKey].impact]);
 								sTotalStats[lsKey]+=parseInt(scanArray[k][lsMap[lsMapKey].impact]);
 								break;
 							}
@@ -599,7 +599,7 @@ function generateSummary() {
 	sHTML+='<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center htfixed2"><i class="fa fa-link fa-2x sumIcon" title="Squad Link"></i><h5 id="shareURL"></h5></div>';
 	sHTML+='<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center htfixed2"><i class="fa fa-dollar fa-2x sumIcon" title="Unit Cost (less Ally)"></i><h5>'+sCost+' Cost</h5></div>';
 	sHTML+='<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center htfixed2"><i class="fa fa-users fa-2x sumIcon" title="Unique Elements"></i><h5>'+sElementCount+' Unique</br>Element(s)</h5></div>';
-	sHTML+='<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center htfixed2"><i class="fa fa-dashboard fa-2x sumIcon" title="Leader STATS"></i><h5>'+JSON.stringify(sTotalStats)+'b </h5></div>';
+	sHTML+='<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center htfixed2"><i class="fa fa-dashboard fa-2x sumIcon" title="Leader STATS"></i><h5>'+JSON.stringify(sTotalStats)+' c</h5></div>';
 	$("#SummarySpace").html(sHTML);
 	/*update state*/
 	var state = { stateSquad: sParam.join() };
