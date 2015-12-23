0d87e42363b9f36fe7c06263a421cca8b009a0b5
Sam GreenP <greenpuck55@gmail.com>
2015-12-23 23:40:00 +1100

    Update bfp.squadbuildbeta.js

--- js/bfp.squadbuildbeta.js
+++ js/bfp.squadbuildbeta.js
@@ -756,7 +756,7 @@ $(document).on("click", '.lsBtns', function(e){
 $(document).on("click", '.unitFound', function(e){
 	e.preventDefault();
 	$('#searchModal').modal('hide');
-	$(unitProcessing).html('<img src="'+rawParseObj[$(this).attr("data-unitid")].img+'" data-unitid="'+$(this).attr("data-unitid")+'" class="unitSelected" title="'+rawParseObj[$(this).attr("data-unitid")].name+" ("+rawParseObj[$(this).attr("data-unitid")].rarity+'*)" />');
+	$(unitProcessing).html('<img src="'+rawParseObj[$(this).attr("data-unitid")].img+'" data-unitid="'+$(this).attr("data-unitid")+'" class="unitSelected" title="'+rawParseObj[$(this).attr("data-unitid")].name+" ("+rawParseObj[$(this).attr("data-unitid")].rarity+'*)" /><kbd class="fRarity">'+rawParseObj[$(this).attr("data-unitid")].rarity+'<i class="fa fa-star"></i></kbd>');
 	refreshALL();
 })
 
