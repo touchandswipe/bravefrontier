4c8eca2c169cda24cb6a2d9cdcb33639e533d317
Sam GreenP <greenpuck55@gmail.com>
2015-12-31 00:42:50 +1100

    Update bfp.squadbuildbeta.js

--- js/bfp.squadbuildbeta.js
+++ js/bfp.squadbuildbeta.js
@@ -1252,7 +1252,7 @@ if (typeof mappedNames !== 'undefined') {
         else
         	unitObj.ls="none";
 	if (valObj["bb"]) {
-		if (valObj[bb][levels][9]) {
+		if (valObj["bb"]["levels"][9]) {
 	        	unitObj.bb=valObj["bb"]["levels"][9];
 	        	unitObj.bbcost=valObj["bb"]["levels"][9]["bc cost"];
 	        	unitObj.bbdc=valObj["bb"]["max bc generated"];
