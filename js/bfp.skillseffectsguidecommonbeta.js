bb2d6edcf620562b26a2b1c88514f3910bf89183
Sam GreenP <greenpuck55@gmail.com>
2015-12-31 13:12:48 +1100

    Update bfp.skillseffectsguidecommonbeta.js

--- js/bfp.skillseffectsguidecommonbeta.js
+++ js/bfp.skillseffectsguidecommonbeta.js
@@ -351,11 +351,13 @@ countVar=0; /*reset count*/
             					})
             				} else if (exKey2=="conditions") {
                 				exSTR+='<div class="row equal"><div class="col-xs-2 col-md-2 bd bg-danger"><span class="ex">'+exKey2+"</span></div>";
+                				exSTR+='<div class="col-xs-10 col-md-10 bi bg-danger">';
                 				for (var x in exVal2) {
                 					$.each(exVal2[x], function(lowKey,lowVal) {
-	                					exSTR+='<div class="col-xs-10 col-md-10 bi bg-danger"><b>'+lowKey+":</b> "+lowVal+'</div></div>';
+	                					exSTR+='<b>'+lowKey+":</b> "+lowVal;
 	            					})
                 				}
+                				exSTR+='</div></div>';
                 			} else {
                 				exSTR+='<div class="row equal"><div class="col-xs-2 col-md-2 bd"><span class="ex">'+exKey2+"</span></div>";
                 				exSTR+='<div class="col-xs-10 col-md-10 bi">'+exVal2+'</div></div>';
