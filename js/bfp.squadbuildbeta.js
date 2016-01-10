e649c0675d9587e8cfbc14881008d9fe870eda8f
Sam GreenP <greenpuck55@gmail.com>
2016-01-11 00:50:39 +1100

    Update bfp.squadbuildbeta.js

--- js/bfp.squadbuildbeta.js
+++ js/bfp.squadbuildbeta.js
@@ -1361,6 +1361,7 @@ function loadUnitSummary(arrayID) {
 		})
 		unitHTML+='</table>';
 	})
+	$("#unitInfoBox").html(unitHTML);
 }
 
 /*fill squad box*/
@@ -1384,6 +1385,7 @@ function parseUnit(slot,rawID) {
 	insertHTML+='<li><b class="text-danger">SPARKmod:</b> <span id="SPARK_'+uRef+'">'+squadSparkDMG+'</span></li>';
 	insertHTML+='<li><b class="text-danger">ELEMENTmod:</b> <span id="ELEMENT_'+uRef+'">'+squadElementDMG+'</span></li>';
 	insertHTML+='<li><b class="text-danger">BBmod:</b> <span id="BB_'+uRef+'">'+squadBBDMG+'</span></li>';
+	insertHTML+='<li>><a href="#" data-id="'++'" class="loadUnitLink">View unit summary</a></li>';
 	insertHTML+='</ul><h6 class="pimptxt"><i class="fa fa-exclamation-circle"></i> stats are max pimped</h6>';
 	$("#stats"+uRef).html(insertHTML);
 }
@@ -1681,6 +1683,12 @@ $(document).on("click", '#calculateDMG', function(e){
 	e.preventDefault();
 	showDMG();
 	$("#damageModal").modal("show");
+})
+
+/*load unit summary*/
+$(document).on("click", '.loadUnitLink', function(e){
+	e.preventDefault();
+	loadUnitSummary($(this).attr("data-id"))
 })
 
 /*MoveinActive skills*/
