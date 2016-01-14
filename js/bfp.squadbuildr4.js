d3563e80b726274bdc2674c4fe90b19a3c04721b
Sam GreenP <greenpuck55@gmail.com>
2016-01-15 09:30:19 +1100

    improves UX on IOS

--- js/bfp.squadbuildr4.js
+++ js/bfp.squadbuildr4.js
@@ -2280,16 +2280,20 @@ $(document).on("click", '#searchNameBtn', function(e){
 
 /*trigger marker*/
 $("body").on("mouseenter", '.lsBtns, .bbBtns, .ubbBtns', function(e){
-    $(this).siblings(".mkBox").show();
+    	$(this).siblings(".mkBox").show();
 }).on("mouseleave", '.lsBtns, .bbBtns, .ubbBtns', function(e){
-    $(this).siblings(".mkBox").hide();
+	var $thatID=$(this);
+	setTimeout(function () {
+        	$thatID.siblings(".mkBox").fadeOut( "slow" );
+    	}, 2000);
 });
 
 /*keep markerbox visible*/
-$("body").on("mouseenter",".mkBox", function(e){
-	$(this).show();
-}).on("mouseleave",".mkBox", function(e){
-	$(this).hide();
+$("body").on("mouseleave",".mkBox", function(e){
+	var $thatID=$(this);
+	setTimeout(function () {
+        	$thatID.siblings(".mkBox").fadeOut( "slow" );
+    	}, 2000);
 }).on("click", ".mkBox", function(e){
 	$(this).siblings("a").toggleClass("marker");
 })
