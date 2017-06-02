ff7fc5ab8fe4e9dbaab045a99f250fe7d7d49605
Sam GreenP <greenpuck55@gmail.com>
2017-06-02 16:44:22 +1000

    Update bfp.skillseffectsguidecommon.js

--- js/bfp.skillseffectsguidecommon.js
+++ js/bfp.skillseffectsguidecommon.js
@@ -499,12 +499,14 @@ if (typeof mappedNames !== 'undefined') {
             $.each(valObj.sbb.levels[0], function(sbbKey,sbbVal) {
                 if (sbbKey!="effects") {
                     sbbSTR+='<div class="row equal"><div class="col-xs-2 col-md-2 bd"><span class="sbb">'+sbbKey+'</span></div>';
-                    if (String(valObj.sbb.levels[0][sbbKey])==String(valObj.sbb.levels[9][sbbKey]))
-                    	sbbSTR+='<div class="col-xs-10 col-md-10 bi">'+valObj.sbb.levels[9][sbbKey]+'</div>';
-                    else
-	                    for (i=0;i<10;i++) {
-				sbbSTR+='<div class="col-xs-1 col-md-1 bi">'+valObj.sbb.levels[i][sbbKey]+'</div>'
-	                    }
+			if (valObj.sbb.levels[0][sbbKey] && valObj.sbb.levels[9][sbbKey]) {
+			    if (String(valObj.sbb.levels[0][sbbKey])==String(valObj.sbb.levels[9][sbbKey]))
+				sbbSTR+='<div class="col-xs-10 col-md-10 bi">'+valObj.sbb.levels[9][sbbKey]+'</div>';
+			    else
+				    for (i=0;i<10;i++) {
+					sbbSTR+='<div class="col-xs-1 col-md-1 bi">'+valObj.sbb.levels[i][sbbKey]+'</div>'
+				    }
+			}
                     sbbSTR+="</div>";
                 } else if (sbbKey=="effects") {
                 	var effCount=0;
